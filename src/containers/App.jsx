30c84ec139d63412aba1f702313865cb27e45800
Rafael Flores <fita500@gmail.com>
2020-11-26 19:40:34 -0600

    Add images and improve styles

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -11,10 +11,16 @@ const App = () => (
     <div className="App">
         <Header />
         <Search />
-        <Categories>
+        <Categories title="Mi Lista">
             <Carousel>
                 <CarouselItem />
                 <CarouselItem />
+                <CarouselItem />
+                <CarouselItem />
+            </Carousel>
+        </Categories>
+        <Categories title="Tendencias">
+            <Carousel>
                 <CarouselItem />
                 <CarouselItem />
             </Carousel>
